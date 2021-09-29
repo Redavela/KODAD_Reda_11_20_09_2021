5dcd120e41d90589f5b9400e8a0f98179a7c8143
Redavela <kodadreda67@gmail.com>
2021-09-29 14:35:30 +0200

    Réglage Error-page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './../css/Error.css';
 
 class Error extends Component {
     render() {
         return (
-            <div>
-                404
-            </div>
+    <div className="error">
+        <main className="error-main">
+            <p className="error-num">404</p>
+            <p className="error-txt">
+            Oops! La page que vous demandez n'existe pas.
+            </p>
+          <Link to="/" title="Home">
+            Retourner sur la page d'accueil
+          </Link>
+        </main>
+    </div>
         );
     }
 }
